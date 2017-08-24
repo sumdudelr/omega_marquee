46d8f6103e77c050de0e5f4735b78ff0c15d22a4
Logan Reich <lareich@live.com>
2017-08-24 13:31:39 -0400

    Removed the check for SegWit as it has now been activated.

--- serialMatrix.py
+++ serialMatrix.py
@@ -16,9 +16,6 @@ response = urllib.urlopen(url)
 data = json.loads(response.read())
 # print(data)
 dock.write("BTC = $" + str(data['market_price_usd']) + "   |   ")
-blocksToSegWit = 481824 - data['n_blocks_total']
-timeToSegWit = blocksToSegWit * data['minutes_between_blocks'] / 60
-dock.write("SegWit activates in " + str(blocksToSegWit) + " blocks or approx. " + "{:.1f}".format(timeToSegWit) + " hours   |   ")
 
 url2 = "http://api.weather.gov/points/39.1056,-84.2416/forecast"
 response2 = urllib.urlopen(url2)
@@ -29,5 +26,4 @@ for period in summary:
 	endTime = dateutil.parser.parse(period['endTime'])
 	if (datetime.datetime.now(pytz.utc) < endTime) and (datetime.datetime.now(pytz.utc) > startTime):
 		dock.write(str(period['detailedForecast']) + "\n")
-		print(type(period['startTime']))
 
