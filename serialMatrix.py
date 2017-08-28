0358f33ace85a6afe57286394f736b59753628aa
Logan Reich <lareich@live.com>
2017-08-28 17:53:07 -0400

    The program now sends the first three characters "OFF" when it is late at night. The Arduino code needs to compare the first three characters to check for "OFF".

--- serialMatrix.py
+++ serialMatrix.py
@@ -9,7 +9,9 @@ import geojson
 dock = serial.Serial(port="/dev/ttyS1", baudrate=57600)
 dock.write("Script Running...\n")
 
-dock.write(datetime.datetime.strftime(datetime.datetime.now(pytz.utc), "%x") + "   |    ")
+now = datetime.datetime.now(pytz.timezone('US/Eastern'))
+
+dock.write(datetime.datetime.strftime(now, "%x") + "   |   ")
 
 url = "https://api.blockchain.info/stats"
 response = urllib.urlopen(url)
@@ -17,7 +19,7 @@ data = json.loads(response.read())
 # print(data)
 dock.write("BTC = $" + str(data['market_price_usd']) + "   |   ")
 
-url2 = "http://api.weather.gov/points/39.1056,-84.2416/forecast"
+url2 = "http://api.weather.gov/points/39.1945,-84.0546/forecast"
 response2 = urllib.urlopen(url2)
 forecast = geojson.loads(response2.read()) 
 summary = forecast['properties']['periods']
@@ -27,3 +29,7 @@ for period in summary:
 	if (datetime.datetime.now(pytz.utc) < endTime) and (datetime.datetime.now(pytz.utc) > startTime):
 		dock.write(str(period['detailedForecast']) + "\n")
 
+offTime = now.replace(hour=21, minute=0)
+onTime = now.replace(hour=7, minute=0)
+if((now > offTime) & (now < onTime)):
+	dock.write('\nOFF')
