183bf5fd5630b0c1f1f07f4ab768356002ace7dd
Logan Reich <lareich@live.com>
2017-08-30 16:50:43 -0400

    I have once again fixed the offtime.

--- serialMatrix.py
+++ serialMatrix.py
@@ -30,7 +30,8 @@ for period in summary:
 		dock.write(str(period['detailedForecast']) + "\n")
 
 offTime = now.replace(hour=21, minute=0)
-onTime = offTime + datetime.timedelta(hours=11)
-if((now > offTime) and (now < onTime)):
-	print('off')
+midnight = offTime + datetime.timedelta(hours=3)
+onTime = now.replace(hour=7, minute=0)
+if(((now > offTime) and (now < midnight)) or ((now > midnight) and (now < onTime))):
+	print('off/')
 	dock.write('\nOFF')
