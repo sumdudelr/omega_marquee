daa1fa6bece393a5e581add325ef5142cc4057e6
Logan Reich <lareich@live.com>
2018-05-05 11:15:25 -0400

    Fixed order of execution, Removed GeoJSON

--- serialMatrix.py
+++ serialMatrix.py
@@ -4,31 +4,30 @@ import datetime
 import serial
 import urllib
 import json
-import geojson
 
 dock = serial.Serial(port="/dev/ttyS1", baudrate=57600)
-dock.write("Script Running...\n")
 
 now = datetime.datetime.now(pytz.timezone('US/Eastern'))
-
-dock.write(datetime.datetime.strftime(now, "%x") + "   |   ")
-
-url = "https://api.blockchain.info/stats"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-# print(data)
-dock.write("BTC = $%(number).2f   |   " % {"number" : data['market_price_usd']})
-
-url2 = "http://api.weather.gov/points/39.1945,-84.0546/forecast"
-response2 = urllib.urlopen(url2)
-forecast = geojson.loads(response2.read()) 
-summary = forecast['properties']['periods']
-for period in summary:
-	startTime = dateutil.parser.parse(period['startTime'])
-	endTime = dateutil.parser.parse(period['endTime'])
-	if (datetime.datetime.now(pytz.utc) < endTime) and (datetime.datetime.now(pytz.utc) > startTime):
-		dock.write(str(period['detailedForecast']) + "\n")
-
 if((now.hour > 21) or (now.hour < 7)):
 	print('off/')
 	dock.write('\nOFF')
+else:
+	dock.write("Script Running...\n")
+	
+	dock.write(datetime.datetime.strftime(now, "%x") + "   |   ")
+
+	url = "https://api.blockchain.info/stats"
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+	# print(data)
+	dock.write("BTC = $%(number).2f   |   " % {"number" : data['market_price_usd']})
+
+	url2 = "http://api.weather.gov/points/39.1945,-84.0546/forecast"
+	response2 = urllib.urlopen(url2)
+	forecast = json.loads(response2.read()) 
+	summary = forecast['properties']['periods']
+	for period in summary:
+		startTime = dateutil.parser.parse(period['startTime'])
+		endTime = dateutil.parser.parse(period['endTime'])
+		if (datetime.datetime.now(pytz.utc) < endTime) and (datetime.datetime.now(pytz.utc) > startTime):
+			dock.write(str(period['detailedForecast']) + "\n")
