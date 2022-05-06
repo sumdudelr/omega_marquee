9368b897c52e058968d4f25e375916d9539e3b2c
Logan Reich <lareich@live.com>
2022-05-06 10:03:56 -0400

    Added Exception Handling
    Now gracefully handles a lack if internet connection

--- Omega_Matrix_Display.py
+++ Omega_Matrix_Display.py
@@ -16,20 +16,26 @@ dateString = now.strftime('%a %b %d, %Y')
 
 dock.write(str.encode(dateString + '   |   '))
 
-url = "https://api.blockchain.info/stats"
-response = urllib.request.urlopen(url)
-data = json.loads(response.read())
-st = "BTC = $%(number).2f   |   " % {"number" : data['market_price_usd']}
-dock.write(str.encode(st))
+try:
+  url = "https://api.blockchain.info/stats"
+  response = urllib.request.urlopen(url)
+  data = json.loads(response.read())
+  st = "BTC = $%(number).2f   |   " % {"number" : data['market_price_usd']}
+  dock.write(str.encode(st))
 
-# Batavia, OH 39.1060,-84.2411  ILN Grid points 44, 38
-url = "https://api.weather.gov/gridpoints/ILN/44,38/forecast"
-response = urllib.request.urlopen(url)
-data = json.loads(response.read())
-summary = data['properties']['periods'][0]
-string = summary['name'] + ': ' + summary['detailedForecast'] + '\0'
+  # Batavia, OH 39.1060,-84.2411  ILN Grid points 44, 38
+  url = "https://api.weather.gov/gridpoints/ILN/44,38/forecast"
+  response = urllib.request.urlopen(url)
+  data = json.loads(response.read())
+  summary = data['properties']['periods'][0]
+  string = summary['name'] + ': ' + summary['detailedForecast']
 
-dock.write(str.encode(string))
-
-
-dock.close()
+  dock.write(str.encode(string))
+  
+except:
+  # Maybe no internet connection?
+  dock.write(str.encode("Internet not available"))
+  
+finally:
+  dock.write(str.encode("\0"))
+  dock.close()
